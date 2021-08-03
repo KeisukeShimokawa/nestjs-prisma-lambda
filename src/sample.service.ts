8313982247534a085eb8fca39c89fe2110deadda
KeisukeShimokawa <shimoke4869@gmail.com>
2021-08-03 22:41:48 +0900

    Prisma経由でデータを取得・作成するサービスを追加

---
+++ src/sample.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@nestjs/common';
+import { Prisma, Sample } from '@prisma/client';
+import { PrismaService } from './prisma.service';
+
+@Injectable()
+export class SampleService {
+  constructor(private prisma: PrismaService) {}
+
+  async sample(
+    sampleWhereUniqueInput: Prisma.SampleWhereUniqueInput,
+  ): Promise<Sample | null> {
+    return this.prisma.sample.findUnique({
+      where: sampleWhereUniqueInput,
+    });
+  }
+
+  async createSample(data: Prisma.SampleCreateInput): Promise<Sample> {
+    return this.prisma.sample.create({
+      data,
+    });
+  }
+}
