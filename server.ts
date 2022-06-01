e2842a9de59bd827e73dde899d87033bec6a1907
shimopino <shimoke4869@gmail.com>
2022-06-02 08:33:21 +0900

    GraphQLのサンプルを追加

---
+++ server.ts
@@ -0,0 +1,20 @@
+import { ApolloServer, gql } from 'apollo-server'
+
+const typeDefs = gql`
+    type Query {
+        greet: String
+
+    }
+`;
+
+const resolvers = {
+    Query: {
+        greet: () => "Hello World"
+    }
+}
+
+const server = new ApolloServer({ typeDefs, resolvers })
+
+server.listen().then(({url}) => {
+    console.log(`Server ready at ${url}`)
+})
