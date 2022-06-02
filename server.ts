12875a8ca8f875c456ab1c0d103067c0d6f167aa
shimopino <shimoke4869@gmail.com>
2022-06-03 07:31:44 +0900

    サンプルデータをもとにResolvesを更新

--- server.ts
+++ server.ts
@@ -1,14 +1,44 @@
 import { ApolloServer, gql } from "apollo-server";
 
+const users = [
+  {
+    id: 1,
+    firstName: "Sample",
+    lastName: "beautify",
+    email: "sample@example.com",
+    password: "12345",
+  },
+  {
+    id: 2,
+    firstName: "Example",
+    lastName: "dirty",
+    email: "dirty@example.com",
+    password: "98765",
+  },
+];
+
 const typeDefs = gql`
   type Query {
-    greet: String
+    users: [User]
+    user(id: ID): User
+  }
+
+  type User {
+    id: ID
+    firstName: String
+    lastName: String
+    email: String
   }
 `;
 
 const resolvers = {
   Query: {
-    greet: () => "Hello World",
+    users: () => users,
+    // @ts-expect-error 型エラーは一旦無視
+    user: (parent, args, context) => {
+      console.log(args);
+      return users.find((item) => item.id === args.id);
+    },
   },
 };
 
