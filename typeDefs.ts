0b1857b324c2c1ab6ccb6161e178b65839902e83
shimopino <shimoke4869@gmail.com>
2022-06-07 07:39:32 +0900

    GraphQLの typeDefs と resolvers を外部ファイルに移す

---
+++ typeDefs.ts
@@ -0,0 +1,24 @@
+import { gql } from "apollo-server";
+
+export const typeDefs = gql`
+type Query {
+    users: [User]
+    user(id: ID): User
+}
+
+input UserInput {
+    firstName: String!
+    lastName: String!
+    email: String!
+    password: String!
+}
+
+type Mutation {}
+
+type User {
+    id: ID
+    firstName: String!
+    lastName: String!
+    email: String!
+}
+`;
